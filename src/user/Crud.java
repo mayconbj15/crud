5e3e4f60e8f5c65eec467d918100adb6aec3c3b7
MarquinhoLiquigas <globaldep90@gmail.com>
2019-03-04 14:42:33 -0300

    Método inserir adicionado Método alterar em construção

--- src/user/Crud.java
+++ src/user/Crud.java
@@ -1,140 +1,167 @@
 package user;
 
 import java.io.*;
-import crud.*;
-import java.util.*;
+import java.util.ArrayList;
 
-public class Crud 
+import crud.Produto;
+import crud.Arquivo;
+
+public class Crud
 {   
    /**
     * Metodo para criar um novo registro de produto
     * @param - arquivo destino
     *
     */
-   public static void inserir(Arquivo arquivo)
+   public static void inserir(Arquivo arquivo) throws IOException
    {  
-	  try 
-	  {
-		  BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
-		  String nome, descricao;
-		  float preco = 0.0F;
+      try 
+      {
+         BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+         String nome, descricao;
+         float preco = 0.0F;
             
-	      System.out.println("\nDigite o nome do produto: ");
-	      nome = br.readLine();
-	            
-	      System.out.println("\nDigite a descrição do produto: ");
-	      descricao = br.readLine();
-	            
-	      System.out.println("\nInforme o preço do produto: ");
-	      preco = Float.parseFloat( br.readLine() );
-	            
-	      Produto produto = new Produto(nome, descricao, preco); 
-	               
-	      arquivo.writeObject(produto);
-	      
-	      System.out.println("Seu produto foi cadastrado com sucasso! :D\n");
-	      
-	  }catch(IOException ioe) {ioe.printStackTrace(); }
-	  
+         System.out.println("\nDigite o nome do produto: ");
+         nome = br.readLine();
+               
+         System.out.println("\nDigite a descrição do produto: ");
+         descricao = br.readLine();
+               
+         System.out.println("\nInforme o preço do produto: ");
+         preco = Float.parseFloat( br.readLine() );
+               
+         Produto produto = new Produto(nome, descricao, preco); 
+                  
+         arquivo.writeObject(produto);
+         
+         System.out.println("Seu produto foi cadastrado com sucasso! :D\n");
+         
+      }
+      catch(IOException ioe) {ioe.printStackTrace(); }
+     
    }//end inserir()
    
    
-   public static void alterar(Arquivo arquivo, int id)
-   {  
-	  try 
-	  {
-		  BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
-		  String nome, descricao;
-		  float preco = 0.0F;
-            
-	      System.out.println("\nDigite o nome do produto: ");
-	      nome = br.readLine();
-	            
-	      System.out.println("\nDigite a descrição do produto: ");
-	      descricao = br.readLine();
-	            
-	      System.out.println("\nInforme o preço do produto: ");
-	      preco = Float.parseFloat( br.readLine() );
-	            
-	      Produto produto = new Produto(nome, descricao, preco); 
-	               
-	      arquivo.writeObject(produto);
-	      
-	      System.out.println("Seu produto foi cadastrado com sucasso! :D\n");
-	      
-	  }catch(IOException ioe) {ioe.printStackTrace(); }
-	  
+   /**
+    * Método para alterar dados do produto(exceto o id).
+    * 
+    * @param arquivo
+    * @param id
+    * @param cod
+    */
+   public static void alterar(Arquivo arquivo, int id, short cod) throws IOException
+   {  	  
+      try 
+      {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         Produto produto = new Produto();
+         produto = arquivo.readObject(id);	   	   
+      	   
+      	   //Bloco de if's para alteracao de atributos
+         if(cod == 1)
+         { 	   		   	   		      
+            System.out.println("Digite o novo nome do produto: ");
+            produto.setNome( br.readLine() ); //Setar novo nome
+         }
+         else if(cod == 2) 
+         {
+            System.out.println("Digite a nova descrição do produto: ");
+            produto.setDescricao( br.readLine() ); //Setar nova descricao
+         }
+         else if(cod == 3) 
+         {
+            System.out.println("Digite o novo preço do produto: ");
+            produto.setPreco( Float.parseFloat( br.readLine() ) ); //Setar novo preco
+         }//end if
+      	   	   	   
+      	   
+      }
+      catch(IOException ioe) { ioe.printStackTrace(); }
+      
    }//end alterar()
+   
+   
+   public static void listar()
+   {
+      
+   }//end listar()
    
    
    public static void main(String [] args) throws IOException
    {
       try
       {
-         //definir dados
+         //definir dados    	 
          BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
          //File file = new File("produto.db");
          Arquivo arquivo = new Arquivo("produtos.db");
-         //ArrayList<Produto> list = new ArrayList<Produto>();
+         ArrayList<Produto> list = new ArrayList<Produto>();
          int selecao = 12;
          System.out.println("Olá, meu nobre!\n");
          
+         //Interface de entrada
+         System.out.println("Qual das seguintes operações o senhor deseja realizar?" + 
+                         "\nDigite: " + 
+                         "\n1 para inclusão;" +
+                         "\n2 para alteração;" +
+                         "\n3 para exclusão;" +
+                         "\n4 para consulta de produtos;" +
+                         "\n5 para listar todos os produtos" +
+                         "\n0 para sair.");
+         
       //TESTAR SE E' PARA SAIR
          while(selecao != 0)
-         {    
-            //Interface de entrada
-            System.out.println("Qual das seguintes operações o senhor deseja realizar?" + 
-                            "\nDigite: " + 
-                            "\n1 para inclusão;" +
-                            "\n2 para alteração;" +
-                            "\n3 para exclusão;" +
-                            "\n4 para consulta de produtos;" +
-                            "\n0 para sair.");
-                             
+         {                                          
             selecao = Integer.parseInt( br.readLine() ); //entrada do codigo de selecao de acao         
-             
-            //INCLUSAO
-            if     (selecao == 1){ inserir(arquivo);  }
-            else if(selecao == 2)
+         
+            if     (selecao == 1){ inserir(arquivo);  }// INCLUSAO
+            else if(selecao == 2)// ALTERACAO
             { 
-            	int id = 0;
-            	System.out.println("Digite o id do produto a ser alterado: ");
-            	id = Integer.parseInt( br.readLine() );
-            	System.out.println("O que deseja alterar no produto?\nDigite:\n");
-            	System.out.println("1 para alterar o nome;");
-            	System.out.println("1 para alterar a descrição;");
-            	System.out.println("1 para alterar o preço;");            
+               int id = 0;
+               short cod = -1; //codigo de selecao
+            	
+               System.out.println("Digite o id do produto a ser alterado: ");
+               id = Integer.parseInt( br.readLine() );
+            	
+            	//testar antes se o id existe
+               if(id > 0 && id == arquivo.getLastID())
+               {
+                  System.out.println("O que deseja alterar no produto?\nDigite:");
+                  System.out.println("1 para alterar o nome;");
+                  System.out.println("2 para alterar a descrição;");
+                  System.out.println("3 para alterar o preço;");
+                  System.out.println("0 para cancelar;");
+               	
+                  cod = Short.parseShort( br.readLine() );
+               	
+                  alterar(arquivo, id, cod);
+               	
+               }
+               else { System.out.println("Id inválido!"); }
+            	            	
+            	
             }
-           // else if(selecao == 3){ remover(arquivo);  } 
-           // else if(selecao == 4){ consultar(arquivo);}
+            // else if(selecao == 3){ remover(arquivo);  } 
+            // else if(selecao == 4){ consultar(arquivo);}
+            else if(selecao == 5) 
+            {
+               list = arquivo.list();
+            
+               for(int i=0; i<list.size(); i++) {
+                  System.out.println(list.get(i));
+               }//end for
+            
+            }//end if
             
          }//end while
          
          System.out.println("Até breve :)");
+         
       } 
       catch(IOException ioe){ ioe.printStackTrace(); }   
          
-   	/*
-      ArrayList<Produto> list = new ArrayList<Produto>();
-   	
+      //arquivo.delete();
       
-      Produto produto = new Produto("Geladeira", "Geladeira Duplex FrostFree 30kg", (float)1200.00);
-      Produto produto1 = new Produto("Ps4", "500gb HD", (float)1800.00);
-      Produto produto2 = new Produto("TV", "4k Full HD Master", (float)1800.00);
-   	
-      
-      arquivo.writeObject(produto1);
-      arquivo.writeObject(produto2);
-   	
-      System.out.println(arquivo.readObject(1));
-   	
-      list = arquivo.list();
-   	
-      for(int i=0; i<list.size(); i++) {
-         System.out.println(list.get(i));
-      }
-      */
-   
    }//end main()
    
 }//end class Main
