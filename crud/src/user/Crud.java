be75255ec9106892a6dd7fec5fc7350384f6a61e
axell-brendow <breno.axel@gmail.com>
2019-03-05 20:46:32 -0300

    Encapsulamento

--- crud/src/user/Crud.java
+++ crud/src/user/Crud.java
@@ -14,6 +14,20 @@ public class Crud
         
     }
     
+    public static Produto readProduct()
+    {
+        String nome, descricao;
+        float preco = 0.0F;
+
+        nome = IO.readLine("\nDigite o nome do produto: ");
+            
+        descricao = IO.readLine("\nDigite a descrição do produto: ");
+            
+        preco = Float.parseFloat( IO.readLine("\nInforme o preço do produto: ") );
+            
+        return new Produto(nome, descricao, preco); 
+    }
+    
     /**
     * Metodo para criar um novo registro de produto
     *
@@ -23,21 +37,8 @@ public class Crud
     {  
         try 
         {
-            String nome, descricao;
-            float preco = 0.0F;
-
-            IO.print("\nDigite o nome do produto: ");
-            nome = IO.readLine();
-                
-            IO.print("\nDigite a descrição do produto: ");
-            descricao = IO.readLine();
-                
-            IO.print("\nInforme o preço do produto: ");
-            preco = Float.parseFloat( IO.readLine() );
-                
-            Produto produto = new Produto(nome, descricao, preco); 
                    
-            arquivo.writeObject(produto);
+            arquivo.writeObject( readProduct() );
 
             IO.println("\nSeu produto foi cadastrado com sucesso! :D\n");
           
@@ -50,21 +51,7 @@ public class Crud
     {  
         try 
         {
-            String nome, descricao;
-            float preco = 0.0F;
-
-            IO.print("\nDigite o nome do produto: ");
-            nome = IO.readLine();
-
-            IO.print("\nDigite a descrição do produto: ");
-            descricao = IO.readLine();
-
-            IO.print("\nInforme o preço do produto: ");
-            preco = Float.parseFloat( IO.readLine() );
-
-            Produto produto = new Produto(nome, descricao, preco); 
-               
-            arquivo.writeObject(produto);
+            arquivo.writeObject( readProduct() );
 
             IO.println("\nSeu produto foi alterado com sucesso! :D\n");
 
@@ -84,27 +71,34 @@ public class Crud
         while(selecao != 0)
         {    
             //Interface de entrada
-            IO.println("Qual das seguintes operações o senhor deseja realizar?" + 
-                        "\nDigite: " + 
-                        "\n1 para inclusão;" +
-                        "\n2 para alteração;" +
-                        "\n3 para exclusão;" +
-                        "\n4 para consulta de produtos;" +
-                        "\n0 para sair.");
-                             
-            selecao = Integer.parseInt( IO.readLine() ); //entrada do codigo de selecao de acao         
+            selecao =
+            Integer.parseInt
+            (
+                IO.readLine
+                (
+                	"Qual das seguintes operações o senhor deseja realizar?" + 
+                    "\nDigite: " + 
+                    "\n1 para inclusão;" +
+                    "\n2 para alteração;" +
+                    "\n3 para exclusão;" +
+                    "\n4 para consulta de produtos;" +
+                    "\n0 para sair." +
+                    "\n" +
+                    "\nOperação: "
+                )
+            ); //entrada do codigo de selecao de acao         
              
             //INCLUSAO
             if      (selecao == 1) { inserir(arquivo); }
             else if (selecao == 2)
             { 
-                int id;
-                IO.println("Digite o id do produto a ser alterado: ");
-                id = Integer.parseInt( IO.readLine() );
-                IO.println("O que deseja alterar no produto?\nDigite:\n");
-                IO.println("1 para alterar o nome;");
-                IO.println("1 para alterar a descrição;");
-                IO.println("1 para alterar o preço;");            
+                int id = Integer.parseInt( IO.readLine("Digite o id do produto a ser alterado: ") );
+                IO.println("O que deseja alterar no produto?");
+                IO.println("Digite:\n");
+                IO.println("1 para alterar o nome");
+                IO.println("2 para alterar a descrição");
+                IO.println("3 para alterar o preço\n");
+                IO.print("Alterar: ");
             }
             // else if(selecao == 3){ remover(arquivo);  } 
             // else if(selecao == 4){ consultar(arquivo);}
